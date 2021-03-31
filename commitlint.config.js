79d5563bd6aad877a9413edda1881c9c8e086bed
ljl <776745393@qq.com>
2021-03-31 22:42:03 +0800

    配置config

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (commit) => {
+      return commit.slice(0, 1).includes(':')
+    }
+  ],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'build'
+      ]
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+}
